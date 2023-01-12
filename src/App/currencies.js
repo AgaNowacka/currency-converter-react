7a693ffac040cbd014e916609cc0ac51c4979ca4
Agnieszka Nowacka <agnesnowacka1@gmail.com>
2023-01-12 09:57:22 +0100

    added currencies, style changes, backgroud added,

---
+++ src/App/currencies.js
@@ -0,0 +1,26 @@
+export const currencies = [
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.6884,
+    },
+
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.5111,
+    },
+
+    {
+        short: "CHF",
+        name: "Frank szwajcarski",
+        rate: 4.7729,
+    },
+
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.4497,
+    },
+
+];
